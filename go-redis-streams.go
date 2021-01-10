81726cfbf03d91ada788a26de2f6d8a937b63213
Steven Rojas <steven.rojas@gmail.com>
2021-01-10 10:14:22 -0400

    Add method to set channels

--- go-redis-streams.go
+++ go-redis-streams.go
@@ -7,6 +7,8 @@ import (
 
 // RedisStreamWrapper interface to handle streams
 type RedisStreamWrapper interface {
+	// SetChannels set the message and error channels
+	SetChannels(messageChan chan interface{}, errChan chan error)
 	// Publish publish data into the stream
 	Publish(message interface{}) (string, error)
 	// Consume consume messages from the stream with a count limit. If 0 it will consume all messages
@@ -14,15 +16,21 @@ type RedisStreamWrapper interface {
 }
 
 type redisStreamWrapper struct {
-	c          *redis.Client
-	stream     string
-	bufferSize int
-	ch         chan interface{} // Channel where the consumed messages are send
-	errChan    chan error
+	c           *redis.Client
+	stream      string
+	bufferSize  int
+	messageChan chan interface{} // Channel where the consumed messages are send
+	errChan     chan error
+}
+
+// SetChannels set the message and error channels
+func (s *redisStreamWrapper) SetChannels(messageChan chan interface{}, errChan chan error) {
+	s.messageChan = messageChan
+	s.errChan = errChan
 }
 
 // Publish publish data into the stream
-func (s redisStreamWrapper) Publish(message interface{}) (string, error) {
+func (s *redisStreamWrapper) Publish(message interface{}) (string, error) {
 	args := redis.XAddArgs{
 		Stream: s.stream,
 		Values: map[string]interface{}{
@@ -34,7 +42,7 @@ func (s redisStreamWrapper) Publish(message interface{}) (string, error) {
 
 // Consume consume messages from the stream with a count limit. If 0 it will consume all messages
 func (s *redisStreamWrapper) Consume(count int64) {
-	s.ch = make(chan interface{}, s.bufferSize)
+	s.messageChan = make(chan interface{}, s.bufferSize)
 	go func() {
 		for {
 			var err error
@@ -56,7 +64,7 @@ func (s *redisStreamWrapper) Consume(count int64) {
 					s.errChan <- err
 					continue
 				}
-				s.ch <- message
+				s.messageChan <- message
 				s.c.XDel(s.stream, element.ID) // Remove consumed message
 			}
 		}
