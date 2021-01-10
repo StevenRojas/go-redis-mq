077d7bc792dc4d528fb572a8308505102c17dbd4
Steven Rojas <steven.rojas@gmail.com>
2021-01-10 09:53:31 -0400

    Consume messages from stream

--- go-redis-streams.go
+++ go-redis-streams.go
@@ -1,6 +1,9 @@
 package goredis
 
-import "github.com/go-redis/redis/v7"
+import (
+	"github.com/go-redis/redis/v7"
+	"github.com/vmihailenco/msgpack/v5"
+)
 
 // RedisStreamWrapper interface to handle streams
 type RedisStreamWrapper interface {
@@ -12,8 +15,8 @@ type redisStreamWrapper struct {
 	c          *redis.Client
 	stream     string
 	bufferSize int
-	limit      int
 	ch         chan interface{}
+	errChan    chan error
 }
 
 // Publish publish data into the stream
@@ -25,4 +28,34 @@ func (s redisStreamWrapper) Publish(message interface{}) (string, error) {
 		},
 	}
 	return s.c.XAdd(&args).Result()
+}
+
+func (s *redisStreamWrapper) Consume(count int64) {
+	s.ch = make(chan interface{}, s.bufferSize)
+	go func() {
+		for {
+			var err error
+			var data []redis.XMessage
+			if count > 0 {
+				data, err = s.c.XRangeN(s.stream, "-", "+", count).Result()
+			} else {
+				data, err = s.c.XRange(s.stream, "-", "+").Result()
+			}
+			if err != nil {
+				s.errChan <- err
+				continue
+			}
+			for _, element := range data {
+				data := []byte(element.Values["data"].(string)) // Get pack message
+				var message map[string]interface{}
+				err := msgpack.Unmarshal(data, message)
+				if err != nil {
+					s.errChan <- err
+					continue
+				}
+				s.ch <- message
+				s.c.XDel(s.stream, element.ID) // Remove consumed message
+			}
+		}
+	}()
 }
--- go.mod
+++ go.mod
@@ -6,4 +6,5 @@ require (
 	github.com/adjust/rmq/v3 v3.0.0
 	github.com/caarlos0/env v3.5.0+incompatible
 	github.com/go-redis/redis/v7 v7.4.0
+	github.com/vmihailenco/msgpack/v5 v5.1.4
 )
